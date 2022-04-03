bd0fac1b9fba7aeb4c6455d91b400a4c28c89604
Danilo Maccari <maccari78@gmail.com>
2022-04-03 05:04:48 -0300

    end of video 04

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { ArgProgComponent } from './components/arg-prog/arg-prog.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BannerComponent } from './components/banner/banner.component';
     HeaderComponent,
     ArgProgComponent,
     SocialComponent,
-    BannerComponent
+    BannerComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
