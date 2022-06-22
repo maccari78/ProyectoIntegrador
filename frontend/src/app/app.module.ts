6fc8a6e00e959931ecbb9f3419e8016f0ac50a1b
Danilo Maccari <maccari78@gmail.com>
2022-06-22 19:41:00 -0300

    login component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -14,6 +14,8 @@ import { HasComponent } from './components/has/has.component';
 import { ProjetsComponent } from './components/projets/projets.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClientModule } from '@angular/common/http';
     EducationComponent,
     HasComponent,
     ProjetsComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- frontend/src/app/components/arg-prog/arg-prog.component.ts
+++ frontend/src/app/components/arg-prog/arg-prog.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-arg-prog',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArgProgComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    this.router.navigate(['/login'])
+  }
 }
